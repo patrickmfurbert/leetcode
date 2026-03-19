0a7af344a050b6bbce2e789dd7fdf76653b37f0a
Patrick Furbert <patrickmfurbert@gmail.com>
2026-03-18 21:49:28 -0400

    added link to leetcode problem

--- csharp/solutions/palindrome.cs
+++ csharp/solutions/palindrome.cs
@@ -1,13 +1,11 @@
+// https://leetcode.com/problems/palindrome-number/description/
+
 public class Palindrome
 {
     public bool solve(int number)
     {
         //convert number to a String
         //convert string to char array
-        //need rules for even length
-        //need rules for odd length
-
-        //if element 0 is equal to negative sign, return false e
         bool isPalindrome = true;
 
         char[] numberArray = number.ToString().ToCharArray();
