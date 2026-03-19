8ea29a2e335c20601dd3e83dd759dc7c3df67031
Patrick Furbert <patrickmfurbert@gmail.com>
2026-03-19 13:47:18 -0400

    updated .gitignore and palindrome.cs

--- csharp/solutions/palindrome.cs
+++ csharp/solutions/palindrome.cs
@@ -5,20 +5,19 @@ public class Palindrome
     public bool solve(int number)
     {
         //convert number to a String
-        //convert string to char array
-        bool isPalindrome = true;
+        string numberArray = number.ToString();
 
-        char[] numberArray = number.ToString().ToCharArray();
-
+        //loop through string numberArray
+        //loop contains two index pointers, i and j starting at opposite ends of the numberArray
+        //check if element at i and element at j equal, if not retunn false
         for (int i = 0, j = numberArray.Length - 1; i <= j; i++, j--)
         {
             if (numberArray[i] != numberArray[j])
             {
-                isPalindrome = false;
-                break;
+                return false;
             }
         }
 
-        return isPalindrome;
+        return true;
     }
 }
